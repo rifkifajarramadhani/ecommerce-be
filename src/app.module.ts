72cdb8fa32e2247f63f13f6d9867949ba3356775
rifkifajarramadhani <rifki@limabean.co.za>
2024-09-23 21:50:48 +0700

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
