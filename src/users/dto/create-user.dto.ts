937def43dd47f66f8b3c72b63ad261b15b81ae82
rifkifajarramadhani <rifkid56@gmail.com>
2024-09-24 15:52:59 +0700

    add validation pipes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsIn, IsNotEmpty } from 'class-validator';
+import { UsersRole } from '../users-role.enum';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsIn([UsersRole.SUPER_ADMIN, UsersRole.CUSTOMER])
+  role: string;
+
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  phoneNumber: string;
+
+  @IsNotEmpty()
+  password: string;
+}
---
+++ src/users/users-role.enum.ts
@@ -0,0 +1,4 @@
+export enum UsersRole {
+  SUPER_ADMIN = 'super_admin',
+  CUSTOMER = 'customer',
+}
